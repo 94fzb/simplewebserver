6179a4cb81da1accaa90e2e9fb119968061a0b64
xiaochun <xchun90@163.com>
2017-02-04 15:15:48 +0800

    add options http method

--- src/main/java/com/hibegin/http/server/impl/HttpMethod.java
+++ src/main/java/com/hibegin/http/server/impl/HttpMethod.java
@@ -3,7 +3,7 @@ package com.hibegin.http.server.impl;
 public enum HttpMethod {
 
     PUT("PUT"), POST("POST"), GET("GET"), DELETE("DELETE"),
-    CONNECT("CONNECT"), HEAD("HEAD"), TRACE("TRACE");
+    CONNECT("CONNECT"), HEAD("HEAD"), TRACE("TRACE"), OPTIONS("OPTIONS");
     private String method;
 
     private HttpMethod(String method) {
