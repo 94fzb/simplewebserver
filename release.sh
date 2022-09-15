c46cdc7d508392feb7b672a8c45e1282a2222957
xiaochun <xchun90@163.com>
2022-09-15 20:57:24 +0800

    update

--- release.sh
+++ release.sh
@@ -1,1 +1,2 @@
+export JDK_JAVA_OPTIONS='--add-opens java.base/java.util=ALL-UNNAMED --add-opens java.base/java.lang.reflect=ALL-UNNAMED --add-opens java.base/java.text=ALL-UNNAMED --add-opens java.desktop/java.awt.font=ALL-UNNAMED'
 ./mvnw release:clean && ./mvnw  release:prepare && ./mvnw release:perform
