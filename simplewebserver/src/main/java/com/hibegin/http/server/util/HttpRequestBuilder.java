a20179043f358787c8dc843fa5daed0d4fe3d598
xiaochun <90xchun@gmail.com>
2025-07-28 12:02:22 +0800

    Fix native image agent error

--- simplewebserver/src/main/java/com/hibegin/http/server/util/HttpRequestBuilder.java
+++ simplewebserver/src/main/java/com/hibegin/http/server/util/HttpRequestBuilder.java
@@ -8,17 +8,27 @@ import com.hibegin.http.server.impl.HttpRequestDecoderImpl;
 
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class HttpRequestBuilder {
 
     public static HttpRequest buildRequest(HttpMethod method, String uri, String host, String userAgent, RequestConfig requestConfig, ApplicationContext applicationContext) throws Exception {
-        String httpHeader = method.toString() + " " + uri + " HTTP/1.1\r\n" +
-                "Host: " + host + "\r\n" +
-                "X-Real-IP: 127.0.0.1\r\n" +
-                "User-Agent: " + userAgent + "\r\n" +
-                "\r\n";
+        StringBuilder httpStr = new StringBuilder(method.toString() + " " + uri + " HTTP/1.1\r\n");
+
+        Map<String, String> headers = new LinkedHashMap<>();
+        headers.put("Host", host);
+        headers.put("User-Agent", userAgent);
+        headers.put("X-Real-IP", "127.0.0.1");
+        if (method == HttpMethod.POST) {
+            headers.put("Content-Length", "0");
+        }
+        for (Map.Entry<String, String> entry : headers.entrySet()) {
+            httpStr.append(entry.getKey()).append(": ").append(entry.getValue()).append("\r\n");
+        }
+        httpStr.append("\r\n");
         HttpRequestDecoderImpl httpRequestDecoder = new HttpRequestDecoderImpl(requestConfig, applicationContext, null);
-        httpRequestDecoder.doDecode(ByteBuffer.wrap(httpHeader.getBytes(Charset.defaultCharset())));
+        httpRequestDecoder.doDecode(ByteBuffer.wrap(httpStr.toString().getBytes(Charset.defaultCharset())));
         return httpRequestDecoder.getRequest();
     }
 }
