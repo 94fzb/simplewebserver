aceed485b7350c71f3c1c93a7079e43ba6493ff7
xiaochun <90xchun@gmail.com>
2025-08-27 15:03:21 +0800

    [shell-release]release version 4.0.109

--- simplewebserver-ssl/src/main/java11/module-info.java
+++ simplewebserver-ssl/src/main/java11/module-info.java
@@ -1,5 +1,6 @@
 module hibegin.simplewebserver.ssl {
     requires java.logging;
+    requires hibegin.simplewebserver;
     exports com.hibegin.common.io.handler.ssl;
     exports com.hibegin.http.server.ssl;
 }
